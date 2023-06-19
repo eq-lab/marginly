98a9802f9bd093dd3bd18666ce17d54d66330f73
Ivan Sanin <rudewalt@gmail.com>
2023-06-19 11:04:44 +0300

    added plugin for verification

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -2,27 +2,29 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 require('hardhat-contract-sizer');
 
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
+import '@matterlabs/hardhat-zksync-deploy';
+import '@matterlabs/hardhat-zksync-solc';
+import '@matterlabs/hardhat-zksync-verify';
 
 // dynamically changes endpoints for local tests
 const zkSyncTestnet =
-  process.env.NODE_ENV == "test"
+  process.env.NODE_ENV == 'test'
     ? {
-      url: "http://localhost:3050",
-      ethNetwork: "http://localhost:8545",
-      zksync: true,
-    }
+        url: 'http://localhost:3050',
+        ethNetwork: 'http://localhost:8545',
+        zksync: true,
+      }
     : {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      zksync: true,
-    };
+        url: 'https://zksync2-testnet.zksync.dev',
+        ethNetwork: 'goerli',
+        zksync: true,
+        verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
+      };
 
 const config: HardhatUserConfig & Record<string, unknown> = {
   zksolc: {
-    version: "1.3.10",
-    compilerSource: "binary",
+    version: '1.3.10',
+    compilerSource: 'binary',
     settings: {
       optimizer: {
         enabled: true,
@@ -30,7 +32,7 @@ const config: HardhatUserConfig & Record<string, unknown> = {
       },
     },
   },
-  defaultNetwork: "zkSyncTestnet",
+  defaultNetwork: 'zkSyncTestnet',
   // defaultNetwork: "hardhat",
   networks: {
     hardhat: {
