c88151acaab88cec984237e2d57cedbff719075b
Pavel <pavel.smelov@equilibrium.io>
2023-06-22 15:59:29 +0300

    uniswap ExactOutput tests

--- packages/router/test/Router.spec.ts
+++ packages/router/test/Router.spec.ts
@@ -42,6 +42,52 @@ describe('MarginlyRouter Uniswap', () => {
     expect(await quoteToken.balanceOf(user.address)).to.be.equal(BigNumber.from(amountToSwap).div(price));
   });
 
+  it('swapExactOutput quote to base', async () => {
+    const { marginlyRouter, quoteToken, baseToken, uniswapPool } = await createMarginlyRouter();
+    const [_, user] = await ethers.getSigners();
+
+    const price = await uniswapPool.price();
+
+    const amountToGet = 1000;
+    const amountToSwap = BigNumber.from(amountToGet).div(price);
+    const initialQuoteAmount = amountToSwap.mul(100);
+    await quoteToken.mint(user.address, initialQuoteAmount);
+    await quoteToken.connect(user).approve(marginlyRouter.address, initialQuoteAmount);
+    expect(await quoteToken.balanceOf(user.address)).to.be.equal(initialQuoteAmount);
+    expect(await baseToken.balanceOf(user.address)).to.be.equal(0);
+
+    await marginlyRouter.connect(user).swapExactOutput(
+      0, quoteToken.address, baseToken.address, initialQuoteAmount, amountToGet
+    );
+
+    expect(await quoteToken.balanceOf(user.address)).to.be.equal(initialQuoteAmount.sub(amountToSwap));
+    expect(await baseToken.balanceOf(user.address)).to.be.equal(amountToGet);
+  });
+
+  it('swapExactOutput base to quote', async () => {
+    const { marginlyRouter, quoteToken, baseToken, uniswapPool } = await createMarginlyRouter();
+    const [_, user] = await ethers.getSigners();
+
+    const price = await uniswapPool.price();
+
+    const amountToGet = 1000;
+    const amountToSwap = BigNumber.from(amountToGet).mul(price);
+    const initialBaseAmount = amountToSwap.mul(100);
+    await baseToken.mint(user.address, initialBaseAmount);
+    await baseToken.connect(user).approve(marginlyRouter.address, initialBaseAmount);
+    expect(await baseToken.balanceOf(user.address)).to.be.equal(initialBaseAmount);
+    expect(await quoteToken.balanceOf(user.address)).to.be.equal(0);
+
+    await marginlyRouter.connect(user).swapExactOutput(
+      0, baseToken.address, quoteToken.address, initialBaseAmount, amountToGet
+    );
+
+    expect(await baseToken.balanceOf(user.address)).to.be.equal(initialBaseAmount.sub(amountToSwap));
+    expect(await quoteToken.balanceOf(user.address)).to.be.equal(amountToGet);
+  });
+});
+
+describe('MarginlyRouter UnknownDex', () => {
   it('swapExactInput UnknownDex', async () => {
     const { marginlyRouter, quoteToken, baseToken } = await createMarginlyRouter();
     const [_, user] = await ethers.getSigners();
