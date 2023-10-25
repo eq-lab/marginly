26128a56e5538c6107ba1c9e16a050cf23ffd3ee
Pavel <pavel.smelov@equilibrium.io>
2023-10-25 14:36:51 +0300

    dodoV1 unit tests

--- packages/router/test/Router.spec.ts
+++ packages/router/test/Router.spec.ts
@@ -621,6 +621,143 @@ describe('MarginlyRouter WooFi', () => {
   });
 });
 
+describe('MarginlyRouter DodoV1', () => {
+  it('swapExactInput 0 to 1, success', async () => {
+    const { marginlyRouter, token0, token1, dodoV1 } = await loadFixture(createMarginlyRouter);
+    const [_, user] = await ethers.getSigners();
+
+    const amountToSwap = 1000;
+    await token0.mint(user.address, amountToSwap);
+    await token0.connect(user).approve(marginlyRouter.address, amountToSwap);
+    expect(await token0.balanceOf(user.address)).to.be.equal(amountToSwap);
+    expect(await token1.balanceOf(user.address)).to.be.equal(0);
+
+    const swapCalldata = constructSwap([Dex.DodoV1], [SWAP_ONE]);
+    await marginlyRouter.connect(user).swapExactInput(swapCalldata, token0.address, token1.address, amountToSwap, 0);
+
+    const price = await dodoV1.pool._BASE_TO_QUOTE_PRICE_();
+
+    expect(await token0.balanceOf(user.address)).to.be.equal(0);
+    const expectedAmount = price.mul(amountToSwap);
+    expect(await token1.balanceOf(user.address)).to.be.equal(expectedAmount);
+  });
+
+  it('swapExactInput 0 to 1, less than minimal amount', async () => {
+    const { marginlyRouter, token0, token1, dodoV1 } = await loadFixture(createMarginlyRouter);
+    const [_, user] = await ethers.getSigners();
+
+    const amountToSwap = 1000;
+    await token0.mint(user.address, amountToSwap);
+    await token0.connect(user).approve(marginlyRouter.address, amountToSwap);
+
+    const price = await dodoV1.pool._BASE_TO_QUOTE_PRICE_();
+    const amountToGetPlusOne = price.mul(amountToSwap).add(1);
+
+    const swapCalldata = constructSwap([Dex.DodoV1], [SWAP_ONE]);
+    await expect(
+      marginlyRouter
+        .connect(user)
+        .swapExactInput(swapCalldata, token0.address, token1.address, amountToSwap, amountToGetPlusOne)
+    ).to.be.revertedWith('SELL_BASE_RECEIVE_NOT_ENOUGH');
+  });
+
+  it('swapExactInput 1 to 0, success', async () => {
+    const { marginlyRouter, token0, token1, dodoV1 } = await loadFixture(createMarginlyRouter);
+    const [_, user] = await ethers.getSigners();
+
+    const price = await dodoV1.pool._BASE_TO_QUOTE_PRICE_();
+    const amountToSwap = 1000;
+    const minAmountOut = BigNumber.from(amountToSwap).mul(9).div(10).div(price);
+
+    const token0BalanceBefore = await token0.balanceOf(dodoV1.pool.address);
+    const token1BalanceBefore = await token0.balanceOf(dodoV1.pool.address);
+
+    await token1.mint(user.address, amountToSwap);
+    await token1.connect(user).approve(marginlyRouter.address, amountToSwap);
+    expect(await token0.balanceOf(user.address)).to.be.equal(0);
+    expect(await token1.balanceOf(user.address)).to.be.equal(amountToSwap);
+
+    const swapCalldata = constructSwap([Dex.DodoV1], [SWAP_ONE]);
+    await marginlyRouter.connect(user).swapExactInput(swapCalldata, token1.address, token0.address, amountToSwap, minAmountOut);
+
+    expect(await token1.balanceOf(user.address)).to.be.equal(0);
+    const expectedAmount = BigNumber.from(amountToSwap).div(price);
+    expect(await token0.balanceOf(user.address)).to.be.equal(expectedAmount);
+
+    expect(await token0.balanceOf(dodoV1.pool.address)).to.be.not.equal(token0BalanceBefore);
+    expect(await token1.balanceOf(dodoV1.pool.address)).to.be.not.equal(token1BalanceBefore);
+  });
+
+  it('swapExactInput 1 to 0, less than minimal amount', async () => {
+    const { marginlyRouter, token0, token1, dodoV1 } = await loadFixture(createMarginlyRouter);
+    const [_, user] = await ethers.getSigners();
+
+    const amountToSwap = 1000;
+    await token1.mint(user.address, amountToSwap);
+    await token1.connect(user).approve(marginlyRouter.address, amountToSwap);
+
+    const price = await dodoV1.pool._BASE_TO_QUOTE_PRICE_();
+    const amountToGetPlusOne = BigNumber.from(amountToSwap).div(price).add(1);
+
+    const swapCalldata = constructSwap([Dex.DodoV1], [SWAP_ONE]);
+    await expect(
+      marginlyRouter
+        .connect(user)
+        .swapExactInput(swapCalldata, token1.address, token0.address, amountToSwap, amountToGetPlusOne)
+    ).to.be.revertedWith('BUY_BASE_COST_TOO_MUCH');
+  });
+
+  it('swapExactOutput 0 to 1', async () => {
+    const { marginlyRouter, token0, token1, dodoV1 } = await loadFixture(createMarginlyRouter);
+    const [_, user] = await ethers.getSigners();
+
+    const price = await dodoV1.pool._BASE_TO_QUOTE_PRICE_();
+
+    const amountToGet = 1000;
+    const amountTransferred = BigNumber.from(amountToGet).div(price).mul(105).div(100);
+    const initialAmount0 = amountTransferred.mul(100);
+    await token0.mint(user.address, initialAmount0);
+    await token0.connect(user).approve(marginlyRouter.address, initialAmount0);
+
+    expect(await token0.balanceOf(user.address)).to.be.equal(initialAmount0);
+    expect(await token1.balanceOf(user.address)).to.be.equal(0);
+
+    const swapCalldata = constructSwap([Dex.DodoV1], [SWAP_ONE]);
+    await marginlyRouter
+      .connect(user)
+      .swapExactOutput(swapCalldata, token0.address, token1.address, amountTransferred, amountToGet);
+
+    expect(await token0.balanceOf(user.address)).to.be.lt(initialAmount0);
+    expect(await token0.balanceOf(user.address)).to.be.gt(initialAmount0.sub(amountTransferred));
+    expect(await token1.balanceOf(user.address)).to.be.equal(amountToGet);
+  });
+
+  it('swapExactOutput 1 to 0', async () => {
+    const { marginlyRouter, token0, token1, dodoV1 } = await loadFixture(createMarginlyRouter);
+    const [_, user] = await ethers.getSigners();
+
+    const price = await dodoV1.pool._BASE_TO_QUOTE_PRICE_();
+
+    const amountToGet = 1000;
+    const amountTransferred = price.mul(amountToGet).mul(105).div(100);
+    const initialAmount1 = amountTransferred.mul(100);
+    await token1.mint(user.address, initialAmount1);
+    await token1.connect(user).approve(marginlyRouter.address, amountTransferred);
+
+    expect(await token0.balanceOf(user.address)).to.be.equal(0);
+    expect(await token1.balanceOf(user.address)).to.be.equal(initialAmount1);
+
+    const swapCalldata = constructSwap([Dex.DodoV1], [SWAP_ONE]);
+    await marginlyRouter
+      .connect(user)
+      .swapExactOutput(swapCalldata, token1.address, token0.address, amountTransferred, amountToGet);
+
+    expect(await token0.balanceOf(user.address)).to.be.equal(amountToGet);
+    expect(await token1.balanceOf(user.address)).to.be.lt(initialAmount1);
+    expect(await token1.balanceOf(user.address)).to.be.gt(initialAmount1.sub(amountTransferred));
+  });
+});
+
 describe('MarginlyRouter DodoV2', () => {
   it('swapExactInput 0 to 1, success', async () => {
     const { marginlyRouter, token0, token1, dodoV2 } = await loadFixture(createMarginlyRouter);
