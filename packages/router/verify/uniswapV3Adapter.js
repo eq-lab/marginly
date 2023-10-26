0f8341e095448d1ce3cd91d40cae8d802cfa0cae
rudewalt <rudewalt@gmail.com>
2023-10-26 17:29:07 +0300

    Deploy/arb mainnet (#117)
    * WIP: config preparing
    
    * change config
    
    * change deploy config
    
    * added deploy result
    
    * hardhatConfig for verification

---
+++ packages/router/verify/uniswapV3Adapter.js
@@ -0,0 +1,32 @@
+module.exports = [
+    [{
+        "token0":"0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+        "token1":"0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        "pool":"0xc31e54c7a869b9fcbecc14363cf510d1c41fa443"
+    },
+    {
+        "token0":"0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+        "token1":"0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        "pool":"0x2f5e87c9312fa29aed5c179e456625d79015299c"
+    },
+    {
+        "token0":"0x912ce59144191c1204e64559fe8253a0e49e6548",
+        "token1":"0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+        "pool":"0xcda53b1f66614552f834ceef361a8d12a0b8dad8"
+    },
+    {
+        "token0":"0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        "token1":"0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a",
+        "pool":"0x1aeedd3727a6431b8f070c0afaa81cc74f273882"
+    },
+    {
+        "token0":"0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8",
+        "token1":"0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        "pool":"0xdbaeb7f0dfe3a0aafd798ccecb5b22e708f7852c"
+    },
+    {
+        "token0":"0x3082cc23568ea640225c2467653db90e9250aaa0",
+        "token1":"0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        "pool":"0x446bf9748b4ea044dd759d9b9311c70491df8f29"
+    }]
+];
