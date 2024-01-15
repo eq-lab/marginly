1c9c2fa6d10fcbf9410361f6dd7d9598fab238bf
Pavel <pavel.smelov@equilibrium.io>
2024-01-15 16:34:40 +0300

    uniswapV3 oracle getPrice methods tests

--- packages/periphery/test/UniswapV3TickOracle.spec.ts
+++ packages/periphery/test/UniswapV3TickOracle.spec.ts
@@ -3,24 +3,43 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import snapshotGasCost from '@uniswap/snapshot-gas-cost';
 import { ethers } from 'hardhat';
 import { createUniswapV3TickOracle, Tokens } from './shared/fixtures';
-import { AbiCoder } from '@ethersproject/abi';
 import { formatUnits } from 'ethers/lib/utils';
-
-function encodeOptions(secondsAgo: number, fee: number) {
-  return ethers.utils.defaultAbiCoder.encode(
-    ['uint16', 'uint24'],
-    [secondsAgo, fee]
-  );
-}
+import { BigNumber } from 'ethers';
 
 describe.only('UniswapV3TickOracle', () => {
-  it('', async () => {
+  it('getBalancePrice forward', async () => {
     const { oracle, pool } = await loadFixture(createUniswapV3TickOracle);
-    const price = await oracle.getBalancePrice(Tokens.USDC, Tokens.WETH, encodeOptions(900, await pool.fee()));
-    console.log(`${price.mul(10 ** 12).div(1n << 96n)}`);
-    console.log(formatUnits(price.div(1n << 96n), 12));
+    const price = await oracle.getBalancePrice(Tokens.USDC, Tokens.WETH);
 
-    console.log(`${await pool.token1ToToken0SqrtPriceX96()}`);
-    console.log(`${await pool.tickCumulativesSecond()}`);
+    const sqrtPrice = await pool.token1ToToken0SqrtPriceX96();
+    const intendedPrice = sqrtPrice.mul(sqrtPrice);
+    expect(price).to.be.closeTo(intendedPrice, intendedPrice.div(10000));
+  });
+
+  it('getMargincallPrice forward', async () => {
+    const { oracle, pool } = await loadFixture(createUniswapV3TickOracle);
+    const price = await oracle.getMargincallPrice(Tokens.USDC, Tokens.WETH);
+
+    const sqrtPrice = await pool.token1ToToken0SqrtPriceX96();
+    const intendedPrice = sqrtPrice.mul(sqrtPrice);
+    expect(price).to.be.closeTo(intendedPrice, intendedPrice.div(10000));
+  });
+
+  it('getBalancePrice backward', async () => {
+    const { oracle, pool } = await loadFixture(createUniswapV3TickOracle);
+    const price = await oracle.getBalancePrice(Tokens.WETH, Tokens.USDC);
+
+    const sqrtPrice = await pool.token1ToToken0SqrtPriceX96();
+    const intendedPrice = BigNumber.from(1n << 96n).mul(1n << 96n).div(sqrtPrice.mul(sqrtPrice));
+    expect(price).to.be.closeTo(intendedPrice, intendedPrice.div(10000));
+  });
+
+  it('getMargincallPrice backward', async () => {
+    const { oracle, pool } = await loadFixture(createUniswapV3TickOracle);
+    const price = await oracle.getMargincallPrice(Tokens.WETH, Tokens.USDC);
+
+    const sqrtPrice = await pool.token1ToToken0SqrtPriceX96();
+    const intendedPrice = BigNumber.from(1n << 96n).mul(1n << 96n).div(sqrtPrice.mul(sqrtPrice));
+    expect(price).to.be.closeTo(intendedPrice, intendedPrice.div(10000));
   });
 });
