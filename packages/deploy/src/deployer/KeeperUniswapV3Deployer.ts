560d57fcca62ba6bf9a0f89d26118637fdcd1a24
rudewalt <rudewalt@gmail.com>
2024-06-21 15:46:11 +0300

    fix deploy script, deploy updated keepers for arbitrum pools (#190)

--- packages/deploy/src/deployer/KeeperUniswapV3Deployer.ts
+++ packages/deploy/src/deployer/KeeperUniswapV3Deployer.ts
@@ -1,20 +1,20 @@
 import { Signer } from 'ethers';
 import { EthOptions } from '../config';
-import { createMarginlyContractReader } from './contract-reader';
+import { createKeeperContractReader } from './contract-reader';
 import { StateStore } from '../common';
 import { Logger } from '../logger';
 import { DeployResult } from '../common/interfaces';
 import { BaseDeployer } from './BaseDeployer';
 
 export class KeeperUniswapV3Deployer extends BaseDeployer {
-  private readonly readMarginlyContract;
+  private readonly readKeeperContract;
 
   public constructor(signer: Signer, ethArgs: EthOptions, stateStore: StateStore, logger: Logger) {
     super(signer, ethArgs, stateStore, logger);
-    this.readMarginlyContract = createMarginlyContractReader();
+    this.readKeeperContract = createKeeperContractReader();
   }
 
   public deployKeeper(): Promise<DeployResult> {
-    return this.deploy('MarginlyKeeperUniswapV3', [], 'marginlyKeeperUniswapV3', this.readMarginlyContract);
+    return this.deploy('MarginlyKeeperUniswapV3', [], 'marginlyKeeperUniswapV3', this.readKeeperContract);
   }
 }
